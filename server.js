a316fff9cb9f25f27f633de543d67ac2b9c52413
Mike Kavouras <kavourasm@gmail.com>
2016-02-05 12:23:00 -0500

    remove that mongo cuz it breaks things

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 var React = require('react');
 var app = require('express')();
 var compression = require('compression');
-var mongoose = require('mongoose');
-var monk = require('monk');
 var routes = require('./config/routes');
-var db = monk('localhost:27017/apt_logger');
 
 app.use(compression())
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
-
-app.use(function(res, req, next) {
-  req.db = db;
-  next();
-});
 
 app.get('/', routes.index);
 
