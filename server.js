0c6aed41c63692ed2f62c4edeac3feb7c04ea91d
mikekavouras <kavourasm@gmail.com>
2016-02-05 12:52:24 -0500

    mongo

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
-var url = 'mongodb://localhost:27017/test';
+var url = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test';
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server.");
